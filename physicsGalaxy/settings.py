77edb35c89589eb4b62bc7b469bf66f45d2481a6
Sudip Bera <70878788+SudipBera083@users.noreply.github.com>
2021-09-13 12:29:27 +0530

    Update settings.py

--- physicsGalaxy/settings.py
+++ physicsGalaxy/settings.py
@@ -127,6 +127,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
 import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
